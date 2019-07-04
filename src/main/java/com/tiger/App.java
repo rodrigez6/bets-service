a8b95200bd71ceb905b32314af52f27f30e50b82
roman <romanchi40160@gmail.com>
2019-07-04 23:14:33 +0300

    Initial commit

--- src/main/java/com/tiger/App.java
+++ src/main/java/com/tiger/App.java
@@ -1,4 +1,13 @@
 package com.tiger;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class App {
+    public static void main(String [] args){
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(App.class);
+        ApplicationContext applicationContext = springApplicationBuilder.run(args);
+    }
 }
